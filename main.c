743e41d2a35f401b3ce87681b81459adbfcf7f2b
HeyitsAp <adrianbalunan63@gmail.com>
2026-09-03 09:51:37 +0200

    Fixed wrong variabel for function, was void

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <math.h>
 
-void Method1(float x, float n);
-void Method2(float x, float n);
-void Method3(float x, int n);
+float Method1(float x, float n);
+float Method2(float x, float n);
+float Method3(float x, int n);
 
 
 int main(){
@@ -14,14 +14,14 @@ int main(){
 }
 
 
-void Method1(float x, float n){
+float Method1(float x, float n){
 	if (n = 1){
 		return x;
 	} else {
 		return x * Method1(x, n-1);
 	}
 }
-void Method2(float x, float n){
+float Method2(float x, float n){
 	if (n = 1){
 		return x;
 	} 
@@ -32,7 +32,7 @@ void Method2(float x, float n){
 		return x*Method2(new_x,new_n);
 	}
 }
-void Method3(float x, int n){
+float Method3(float x, int n){
 	return pow(x, n);
 }
 

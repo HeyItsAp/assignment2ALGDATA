6df57580ef9a5623a4e28fa5623fde4b2d64a8c4
HeyitsAp <adrianbalunan63@gmail.com>
2026-09-04 14:26:03 +0200

    Added missing

--- main.c
+++ main.c
@@ -11,6 +11,10 @@ float Method3(float x, int n);
 int main(){
 	float x = 5.0;
 	int exponent = 11;
+	printf("Method1: %f\n", Method1(x, exponent));
+	printf("Method2: %f\n", Method2(x, exponent));
+	printf("Method3: %f\n", Method3(x, exponent));
+
 }
 
 
@@ -26,9 +30,9 @@ float Method2(float x, float n){
 		return x;
 	} 
 
-	if (n & 1){
-		float n,, new_x = x*x
-		float new_n = (n-1)/2
+	if (n & 1.0){
+		float new_x = x*x;
+		float new_n = (n-1)/2;
 		return x*Method2(new_x,new_n);
 	}
 }
